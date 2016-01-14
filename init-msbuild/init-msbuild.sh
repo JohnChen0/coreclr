a3dceb5c523d5d8462c13387cfcba6a91aac9f0b
John Chen <jochen@microsoft.com>
2016-01-14 14:47:47 -0800

    Move closer to joperezr/buildtools

--- init-msbuild/init-msbuild.sh
+++ init-msbuild/init-msbuild.sh
@@ -1,23 +1,47 @@
-#!/usr/bin/env bash
-
-__scriptpath=$(cd "$(dirname "$0")"; pwd -P)
 __DOTNET_CMD=$1
-__TOOLS_DIR=$2
+__MSBUILDRUNTIME_DIR=$2
+__TOOLSDIR=$3
+__BUILDTOOLS_PACKAGE_DIR=$(cd "$(dirname "$0")"; pwd -P)
+__MSBUILD_CONTENT_JSON="{\"dependencies\": {\"Microsoft.Portable.Targets\": \"0.1.1-dev\"},\"frameworks\": {\"dnxcore50\": {},\"net46\": {}}}"
 
-__BUILDERRORLEVEL=0
+OSName=$(uname -s)
+case $OSName in
+  Darwin)
+    __PUBLISH_RID=osx.10.10-x64
+    ;;
+
+  Linux)
+    __PUBLISH_RID=ubuntu.14.04-x64
+    ;;
+
+  *)
+    echo "Unsupported OS $OSName detected. Downloading ubuntu-x64 tools"
+    __PUBLISH_RID=ubuntu.14.04-x64
+    ;;
+esac
 
 if [ ! -e "$__DOTNET_CMD" ]; then
-   echo "ERROR: Cannot find dotnet at path '$__DOTNET_CMD'. Please pass in the path to dotnet as the 1st parameter."
-   exit 1
+  echo "ERROR: Cannot find the dotnet.exe at path '$__DOTNET_CMD'. Please pass in the path to dotnet.exe as the 1st parameter"
+  exit 1
 fi
 
-if [ ! -e "$__TOOLS_DIR" ]; then
-   echo "ERROR: Cannot find Tools directory at path '$__TOOLS_DIR'. Please pass in the Tools directory as the 2nd parameter."
-   exit 1
+if [ -z "$__MSBUILDRUNTIME_DIR" ]; then
+  echo "ERROR: MSBuild Runtime Directory is required for the restore. Please pass in the path where MSBuild will be restored as the 2nd parameter."
+  exit 1
 fi
 
-cd $__scriptpath
-$__DOTNET_CMD restore --source $__scriptpath/NuGet/ --source https://www.myget.org/F/dotnet-core/ --source https://www.myget.org/F/dotnet-buildtools/ --source https://www.nuget.org/api/v2/
-$__DOTNET_CMD publish -f dnxcore50 -o $__TOOLS_DIR
+if [ ! -d "$__TOOLSDIR" ]; then
+  echo "ERROR: Cannot find the buildtools location at '$__TOOLSDIR'. Please pass in the path to buildtools as the 3rd parameter."
+  exit 1
+fi
 
-exit $__BUILDERRORLEVEL
+# Restoring most of the runtime
+cd "$__BUILDTOOLS_PACKAGE_DIR/msbuild-runtime/"
+"$__DOTNET_CMD" restore "$__BUILDTOOLS_PACKAGE_DIR/msbuild-runtime/project.json" --source $__TOOLSDIR/../init-msbuild/NuGet/ --source https://www.myget.org/F/roslyn-nightly/ --source https://www.myget.org/F/dotnet-core/ --source https://www.myget.org/F/dotnet-buildtools/ --source https://www.nuget.org/api/v2/
+"$__DOTNET_CMD" publish -f dnxcore50 -r ${__PUBLISH_RID} -o "$__MSBUILDRUNTIME_DIR"
+chmod a+x "$__MSBUILDRUNTIME_DIR/corerun"
+
+# Copy buildtools over to msbuild dir since msbuild currently can't load an external task library.
+cp -n $__TOOLSDIR/* $__MSBUILDRUNTIME_DIR
+
+exit 0
